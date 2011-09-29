275c5c0ee92cefd16c674c0b9b4ca2508297e088
Elis Axelsson <elis.axelsson@gmail.com>
2011-09-29 08:51:08 +0200

    Added Doxygen docs for the basic controller class

--- classes/class_basic_controller.php
+++ classes/class_basic_controller.php
@@ -1,6 +1,19 @@
 <?php
 
+/**
+ * BasicController class, Baseclass for all Controllers, handles View loading and Model loading.
+ *
+ * @author Elis Axelsson <elis.axelsson@gmail.com>
+ * @copyright GNU Public License v3
+ */
+
 class BasicController extends BasicObject {
+	/**
+	 * Construction of class...
+	 * 
+	 * @param $uri The UriParser contains much usefull info to send on from here
+	 * @param $db  The Database instance is never bad to have
+	 */
 	public function __construct(UriParser &$uri, Db &$db) {
 		$structure = array('uri' => 'Object');
 		parent::__construct($db, $structure, False);
@@ -8,6 +21,12 @@ class BasicController extends BasicObject {
 		$this->uri = $uri;
 	}
 	
+	/**
+	 * Handles loading of Model objects
+	 * 
+	 * @param $name modelname
+	 * @returns A Model Object
+	 */
 	public function model($name) {
 		$file = '../models/'.strtolower($name).'.php';
 		if(file_exists($file)) {
@@ -20,6 +39,12 @@ class BasicController extends BasicObject {
 			throw new Exception('No Model named: '.$name);
 	}
 	
+	/**
+	 * Handles loading of the View object
+	 * 
+	 * @param $name Name of your viewfile of choice
+	 * @returns A View object prepared with your viewfile
+	 */
 	public function view($name) {
 		return new View($name, $this->db);
 	}
