3677c3364e44dcefc0c2b9cb375544d016484435
Elis Axelsson <elis.axelsson@gmail.com>
2011-09-29 08:32:53 +0200

    Added some doxygen docs to the class

--- classes/class_view.php
+++ classes/class_view.php
@@ -1,6 +1,19 @@
 <?php
 
+/**
+ * View class, handles data and renders a view file
+ *
+ * @author Elis Axelsson <elis.axelsson@gmail.com>
+ * @copyright GNU Public License v3
+ */
+
 class View extends BasicObject {
+	/**
+	 * Construction of class...
+	 * 
+	 * @param $name Name of the view you want to load
+	 * @param $db Database instance
+	 */
 	public function __construct($name, Db &$db) {
 		$structure = array('data' => 'array', 'name' => 'string');
 		parent::__construct($db, $structure, True);
@@ -20,11 +33,11 @@ class View extends BasicObject {
 	}
 	
 	public function render() {
-		if(count($this->data['data']) >= 1) ///< @todo This block causes 500 internal server error if I don't give it data at all
+		if(count($this->data['data']) >= 1) /// @todo This block causes 500 internal server error if I don't give it data at all
 			foreach($this->data['data'] as $key => $value)
 				$$key = $value;
 		
-		$_SERVER['DOCUMENT_ROOT'] = str_replace('http', 'views', $_SERVER['DOCUMENT_ROOT']); ///< @todo replace the end of the string, not in the string
+		$_SERVER['DOCUMENT_ROOT'] = str_replace('http', 'views', $_SERVER['DOCUMENT_ROOT']); /// @todo replace the end of the string, not in the string
 		
 		$_view_file = $this->data['file'];
 		unset($this);
