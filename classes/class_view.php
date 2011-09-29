7d8988d409223be8c4a0ab6b7b1d64491612dd5f
Elis Axelsson <elis.axelsson@gmail.com>
2011-09-29 08:35:22 +0200

    Now documented the full class

--- classes/class_view.php
+++ classes/class_view.php
@@ -2,7 +2,7 @@
 
 /**
  * View class, handles data and renders a view file
- *
+ * 
  * @author Elis Axelsson <elis.axelsson@gmail.com>
  * @copyright GNU Public License v3
  */
@@ -28,10 +28,19 @@ class View extends BasicObject {
 		$this->data['file'] = $file;
 	}
 	
+	/**
+	 * Add data you want the viewfile should be able to access, such as arrays, objects, strings and so on
+	 * 
+	 * @param $key Name of the variable as you want it
+	 * @param $value Value of the variable
+	 */
 	public function addData($key, $value) {
 		$this->data['data'][$key] = $value;
 	}
 	
+	/**
+	 * Just renders the page
+	 */
 	public function render() {
 		if(count($this->data['data']) >= 1) /// @todo This block causes 500 internal server error if I don't give it data at all
 			foreach($this->data['data'] as $key => $value)
