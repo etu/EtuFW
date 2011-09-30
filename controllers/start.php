c6c5c168da656572275d7ebec62da7dd75b70c16
Elis Axelsson <elis.axelsson@gmail.com>
2011-09-30 08:36:00 +0200

    Made the controller and model a bit more strict on the input data and added doxygen comments

--- controllers/start.php
+++ controllers/start.php
@@ -1,10 +1,28 @@
 <?php
 
+/**
+ * ControllerStart is one of the simplest controller you can make, this is just a example.
+ * 
+ * @author Elis Axelsson <elis.axelsson@gmail.com>
+ * @copyright GNU Public License v3
+ * @package Application
+ */
+
 class ControllerStart extends BasicController {
-	public function __construct(&$uri, &$db) {
+	/**
+	 * Use this constructor when you build your own class, you can extend it if you want,
+	 * but this structure is required.
+	 * 
+	 * @param $uri UriParser instance
+	 * @param $db  Db instance
+	 */
+	public function __construct(UriParser &$uri, Db &$db) {
 		parent::__construct($uri, $db);
 	}
 	
+	/**
+	 * This is the action "index" as in, /index.php/start/index in this case.
+	 */
 	public function index() {
 		$model = parent::model('example');
 		$text = $model->sayHello();
--- models/example.php
+++ models/example.php
@@ -1,11 +1,25 @@
 <?php
 
+/**
+ * ModelExample, Very simple example for a model, you got the Db class availble. Use it wisely.
+ * 
+ * @author Elis Axelsson <elis.axelsson@gmail.com>
+ * @copyright GNU Public License v3
+ * @package Application
+ */
+
 class ModelExample {
-	function __construct() {
-		$this->db = new Db;
+	/**
+	 * Do not mind the constructor too much... It provides you the Db instance, use it.
+	 */
+	public function __construct(Db &$db) {
+		$this->db = $db;
 	}
 	
-	function sayHello() {
+	/**
+	 * Example method which you call from the controller.
+	 */
+	public function sayHello() {
 		return "Hello from the example model!";
 	}
 }
